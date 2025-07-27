65d23a3dfc261bbed1063a1c92d787bd8bb408cb
sajan oli <sajanoli080@gmail.com>
2025-07-27 19:09:23 +0545

    View model added

---
+++ ViewModel/CommentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.ViewModel
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+        [StringLength(150, ErrorMessage = "Comment cannot be longer than 150 characters.")]
+        public string Text { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
---
+++ ViewModel/TaskItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.ViewModel
+{
+    public class TaskItemViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Title cannot be longer than 30 characters.")]
+        public string Title { get; set; }
+        [StringLength(60, ErrorMessage = "Title cannot be longer than 60 characters.")]
+        public string Description { get; set; }
+        public string Type { get; set; }
+        [Required]
+        [Display(Name = "Is Task completed")]
+        public bool IsCompleted { get; set; }
+
+    }
+}
